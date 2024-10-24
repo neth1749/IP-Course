3b0a38bb8b7e772d44ac52d97bdb8041314dda63
sreyneth <thornsreyneth1@gmail.com>
2024-10-24 09:06:46 +0700

    TP1: Finish step 3

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script>
+</script>
+
+<template>
+  <header>
+
+    <div class="wrapper"> This is my first vueJs  project.
+      
+    </div>
+  </header>
+
+  <main>
+    <TheWelcome />
+  </main>
+</template>
+
+
+<style></style>
