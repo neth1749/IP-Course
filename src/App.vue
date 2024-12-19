05b3a611523a0f6f15a004c2597a4208bd0ee6c4
Thorn sreyneth <161791889+neth1749@users.noreply.github.com>
2024-12-19 08:57:49 +0700

    exercise1,exercise2

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+
+  <div class="container">
+    <Hompage/>
+  </div>
+
+</template>
+
+<script>
+
+import Hompage from './components/Hompage.vue';
+export default{
+  components:{
+    Hompage,
+  }
+}
+</script>
+
+<style>
+</style>
